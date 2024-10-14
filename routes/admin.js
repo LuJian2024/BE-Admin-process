29ec9c6d69b0b8d1fc35bcb41af8f119d6c91955
JianLu <jian.lu@dci-student.org>
2024-10-14 19:48:52 +0200

    refactor: Update admin route logic for filtering users by role

--- routes/admin.js
+++ routes/admin.js
@@ -24,18 +24,19 @@ router.get("/users", authorizeJwt, isAdmin, async (req, res) => {
     try {
         const { role, exact } = req.query; // exact 参数决定是否要精确匹配
         const filter = {};
-
+        console.log(role, exact);
         // 检查是否有多个角色
         if (Array.isArray(role)) {
             // 如果需要精确匹配且包含一个角色
-            if (exact === "true") {
-                filter.roles = {
-                    $size: 1,
-                    $elemMatch: { $in: role },
-                };
-            }
+            // if (exact === "true") {
+            //     filter.roles = {
+            //         $size: 1,
+            //         $elemMatch: { $in: role },
+            //     };
+            // }
             // 如果需要查找同时包含多个角色
-            else if (role.includes("user") && role.includes("admin")) {
+
+            if (role.includes("user") && role.includes("admin")) {
                 filter.roles = { $all: ["user", "admin"] };
             } else {
                 filter.roles = { $in: role };
@@ -48,7 +49,7 @@ router.get("/users", authorizeJwt, isAdmin, async (req, res) => {
                     $elemMatch: { $eq: role },
                 };
             } else {
-                filter.roles = { $in: [role] };
+                filter.roles = { $in: [role] }; //$in 操作符后面必须是一个数组格式。它的作用是检查指定字段的值是否在提供的数组中匹配任何一个元素。
             }
         }
         // filter.roles = { $ne: "admin" }; // 过滤掉 admin
@@ -132,7 +133,17 @@ router
 // CRUD-operations for products
 router.get("/products", authorizeJwt, isAdmin, async (req, res) => {
     try {
-        const products = await Product.find();
+        const limit = Number(req.query.limit) || 2;
+        const page = Number(req.query.page) || 0;
+        const sortDirection = req.query.sortDirection === "asc" ? 1 : -1;
+        const sortBy = req.query.sortBy || "price";
+
+        const products = await Product.find({ category: req.query.category })
+            .limit(limit)
+            .skip(page * limit) // 正确的分页逻辑，因为 page 是从 0 开始的，所以需要乘以 limit
+            .sort({
+                [sortBy]: sortDirection,
+            });
         return res.status(200).json({ products });
     } catch (error) {
         res.status(500).json({ message: "Server error" });
@@ -237,35 +248,46 @@ router
             }
 
             const updateFields = {};
+
+            // 检查是否需要更新订单状态
             if (
                 existingOrder.shippingResult.status === "processing" &&
                 existingOrder.paymentResult.status === "succeeded"
             ) {
                 updateFields.orderStatus = "paid";
-            }
-            if (
+            } else if (
                 existingOrder.shippingResult.status === "succeeded" &&
                 existingOrder.paymentResult.status === "succeeded"
             ) {
                 updateFields.orderStatus = "shipped";
-            }
-
-            if (existingOrder.paymentResult.status === "cancelled") {
+            } else if (existingOrder.paymentResult.status === "cancelled") {
                 updateFields.orderStatus = "cancelled";
-                updateFields.shippingResult.status = "cancelled";
+
+                // // 确保 shippingResult 存在
+                // if (!existingOrder.shippingResult) {
+                //     existingOrder.shippingResult = {}; // 初始化为一个空对象
+                // }
+                updateFields.shippingResult = {
+                    status: "cancelled",
+                };
             }
 
+            // 记录更新字段
             const updatedOrder = await Order.findByIdAndUpdate(
                 req.params.id,
-                {
-                    $set: updateFields,
-                },
+                { $set: updateFields },
                 { new: true }
             );
 
-            return res
-                .status(200)
-                .json({ message: "Order updated successfully", updatedOrder });
+            // 检查是否成功更新订单
+            if (!updatedOrder) {
+                return res.status(404).json({ message: "Order update failed" });
+            }
+
+            return res.status(200).json({
+                message: "Order updated successfully",
+                orderStatus: updatedOrder.orderStatus,
+            });
         } catch (error) {
             res.status(500).json({ message: "Server error" });
         }
@@ -297,36 +319,8 @@ router
     });
 export default router;
 
-// {
-//     "name": "Charlie",
-//     "email": "charlie@example.com",
-//     "password": "hashed_password_789",
-//     "roles": ["admin"]
-// }
-
 //------------products---------------------------
-// {
-//     "name": "Wireless Headphones",
-//     "image": "https://example.com/images/headphones.jpg",
-//     "brand": "BrandB",
-//     "category": "Electronics",
-//     "description": "Comfortable wireless headphones with noise-cancelling feature.",
-//     "price": : "199.99",
-//     "countInStock": 100,
-//     "rating": 4.7,
-//     "numReviews": 25
-//   },
-//   {
-//     "name": "Smartphone",
-//     "image": "https://example.com/images/smartphone.jpg",
-//     "brand": "BrandC",
-//     "category": "Mobile",
-//     "description": "A sleek smartphone with a powerful processor and long battery life.",
-//     "price":  "999.99",
-//     "countInStock": 30,
-//     "rating": 4.6,
-//     "numReviews": 40
-//   },
+
 //   {
 //     "name": "Smartwatch",
 //     "image": "https://example.com/images/smartwatch.jpg",
@@ -340,56 +334,6 @@ export default router;
 //   }
 
 //------------orders---------------------------
-// {
-//     "orderItems": [
-//       {
-//         "name": "Wireless Mouse",
-//         "qty": 2,
-//         "price": 29.99,
-//         "product": "670c04d46a31e2c8ba34e43a"
-//       }
-//     ],
-//     "amount": {
-//       "itemsPrice": {
-//         "$numberDecimal": "59.98"
-//       },
-//       "shippingPrice": {
-//         "$numberDecimal": "5.00"
-//       },
-//       "taxPrice": {
-//         "$numberDecimal": "3.00"
-//       },
-//       "totalPrice": {
-//         "$numberDecimal": "67.98"
-//       }
-//     },
-//     "orderStatus": "shipped",
-//     "shippingAddress": {
-//       "fullName": "David Lee",
-//       "phone": "111-555-7890",
-//       "address": {
-//         "street": "123 Oak Street",
-//         "city": "Los Angeles",
-//         "postalCode": "90001",
-//         "country": "USA"
-//       }
-//     },
-//     "shippingResult": {
-//       "status": "succeeded",
-//       "deliveredAt": "2024-10-05T12:00:00.000Z"
-//     },
-//     "paymentResult": {
-//       "paymentMethod": "PayPal",
-//       "id": "payment_id_7",
-//       "status": "succeeded",
-//       "update_time": "2024-10-03T11:00:00Z",
-//       "email_address": "david.lee@example.com",
-//       "paidAt": "2024-10-03T11:00:00.000Z"
-//     },
-//     "user": "670c04d46a31e2c8ba34e442",
-//     "createdAt": "2024-10-02T14:00:00.000Z",
-//     "updatedAt": "2024-10-05T12:00:00.000Z"
-//   }
 
 // {
 //     "orderItems": [
